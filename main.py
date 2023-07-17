e34e96d135cb697d5c956fbf9a6c307263bfb46a
lukikow <lukasz@kowalczyk.im>
2023-07-17 19:36:34 +0200

    Refactored to object oriented approach.

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
-def isBeaten (user_input, bot_input) -> bool :
-    result = False
 
-    match user_input:
-        case "rock" :
-            if bot_input == "scissor" :
-                result = True
-        case "paper" :
-            if bot_input == "rock" :
-                result = True
-        case "scissor" :
-            if bot_input == "rock" :
-                result = True
-    
-    return result
+import random
 
-rock_vs_paper = isBeaten("rock", "scissor")
-print (f"Does rock beats paper? {rock_vs_paper}")
+
+class RockPaperScissors (object) :
+    def __init__(self, user_input) -> None:
+        self.available_choices = ("rock", "paper", "scissors")
+        self.bot_input = random.choice(self.available_choices)
+        self.user_input = user_input.lower()
+        self.result = False
+        
+    def compare_choices (self) -> None :
+        match self.user_input:
+            case "rock" :
+                if self.bot_input == "scissors" :
+                    self.result = True
+            case "paper" :
+                if self.bot_input == "rock" :
+                    self.result = True
+            case "scissors" :
+                if self.bot_input == "paper" :
+                    self.result = True
+            case other :
+                print (f"Choose one of following: {self.available_choices}")
+
+    def print_result (self) -> None:
+        message = "Congratulation. You win!"
+        
+        if not self.result :            
+            message = "You lose!"
+            if self.user_input == self.bot_input :
+                message = "Draw!"
+
+            message += f" Bot choice was {self.bot_input}"
+        
+        print (message)
+
+def lets_play(user_input):
+    new_game = RockPaperScissors(user_input)
+
+    new_game.compare_choices()
+    new_game.print_result()
